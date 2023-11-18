28be06f7dd3660581a7dc2cbb8cf5d06bab7654a
Stromwasser <144160062+Stromwasser@users.noreply.github.com>
2023-11-18 19:42:55 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace DNSAuflösungHTMLTags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(91, 57);
+            button1.Name = "button1";
+            button1.Size = new Size(114, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Textdatei öffnen";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(322, 59);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Abbrechen";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(535, 61);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Reinigen";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(121, 132);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(360, 132);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Ip-Adresse(n)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(636, 132);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Tags";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(24, 162);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(221, 276);
+            textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(292, 162);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ReadOnly = true;
+            textBox2.ScrollBars = ScrollBars.Vertical;
+            textBox2.Size = new Size(213, 276);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(553, 162);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.Size = new Size(207, 276);
+            textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(951, 640);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DNSAuflösungHTMLTags
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Logik für "Textdatei öffnen"
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //textBox1.Text = ofd.FileName;
+
+                string filePath = ofd.FileName;
+                string[] addresses = File.ReadAllLines(filePath);
+
+                foreach (string address in addresses)
+                {
+                    // Запускаем задачу для каждого адреса
+                    Task.Run(() => ProcessAddress(address)).Wait();
+                }
+            }
+            else
+            {
+                textBox1.Text = "Keine Datei ausgewählt";
+            }
+        }
+
+        private void ProcessAddress(string address)
+        {
+            try
+            {
+              
+                IPHostEntry host = Dns.GetHostEntry(address);
+
+               string dnsAddress = address;
+                UpdateTextBox1($"{dnsAddress}");
+
+               string ipAddresses = string.Join(", ", host.AddressList.Select(ip => ip.ToString()));
+                UpdateTextBox2($"{ipAddresses}");
+
+
+                using (HttpClient client = new HttpClient())
+                {
+                    string html = client.GetStringAsync($"http://{address}").Result;
+
+                    UpdateTextBox3($"{html}");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Обрабатываем ошибку, например, выводим ее в консоль
+                Console.WriteLine($"Error processing {address}: {ex.Message}");
+            }
+        }
+        
+        private void UpdateTextBox1(string text)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.BeginInvoke((MethodInvoker)delegate
+                {
+                    textBox1.Text += text;
+                });
+            }
+            else
+            {
+                textBox1.Text += text;
+            }
+        }
+        private void UpdateTextBox2(string text)
+        {
+            if (textBox2.InvokeRequired)
+            {
+                textBox2.BeginInvoke((MethodInvoker)delegate
+                {
+                    textBox2.Text += text;
+                });
+            }
+            else
+            {
+                textBox2.Text += text;
+            }
+        }
+
+        private void UpdateTextBox3(string text)
+        {
+            if (textBox3.InvokeRequired)
+            {
+                textBox3.BeginInvoke((MethodInvoker)delegate
+                {
+                    textBox3.Text += text;
+                });
+            }
+            else
+            {
+                textBox3.Text += text;
+            }
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Logik für "Abbrechen"
+            // Code
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Logik für "Reinigen"
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+        }
+    }
+}
